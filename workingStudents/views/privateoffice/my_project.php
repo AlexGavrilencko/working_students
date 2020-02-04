61995200f22bd131aff704e2dffc5a455f937cb3
VarvaraTu <vtulenceva98@gmail.com>
2020-02-04 21:00:42 +0700

    hghgh

--- workingStudents/views/privateoffice/my_project.php
+++ workingStudents/views/privateoffice/my_project.php
@@ -18,31 +18,37 @@ $this->title = 'Мои достижения';
                 <p>
                     <?= Html::a('Добавить фотографию', ['privateoffice/set-project'], ['class' => 'btn btn-rounded btnred']) ?>
                 </p>
-                    <div class="border_project">    
+                      
                         <!-- результаты поиска -->
-                        <div class="row">
+                        
                             <?php
                                 foreach ($project as $pr)
                                 : 
                             ?>
-                                <div class="col">
-                                    <!-- для изображения -->
-                                    <?php if($pr->image): ?>
-                                        <img class="project_img" src="/uploads/<?= $pr->image?>" alt="">
-                                        <div class="row">
-                                            <div class="col-4">
-                                                <a href="<?= Url::toRoute(['privateoffice/set-project', 'id'=>$pr->id]); ?>"><img class="pencil" src="/public/img/pencil.png" alt="Редактировать"></a>
-                                            </div>
-                                            <div class="col-4">
-                                                <a href="<?= Url::toRoute(['privateoffice/project_del', 'id'=>$pr->id]); ?>"><img class="trashcan" src="/public/img/trashcan.png" alt="Удалить"></a>
-                                            </div>
-                                        </div>
-                                    <?php endif; ?>
+                            <div class="border_project">
+                                <div class="row justify-content-center">
+                                    
+                                        <div class="col-8 col-sm-10 col-md-6 col-lg-6 col-xl-4">
+                                            <!-- для изображения -->
+                                            <?php if($pr->image): ?>
+                                                <img class="project_img" src="/uploads/<?= $pr->image?>" alt="">
+                                                <div class="row">
+                                                    <div class="col-4">
+                                                        <a href="<?= Url::toRoute(['privateoffice/set-project', 'id'=>$pr->id]); ?>"><img class="pencil" src="/public/img/pencil.png" alt="Редактировать"></a>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <a href="<?= Url::toRoute(['privateoffice/project_del', 'id'=>$pr->id]); ?>"><img class="trashcan" src="/public/img/trashcan.png" alt="Удалить"></a>
+                                                    </div>
+                                                </div>
+                                            <?php endif; ?>
+                                        </div> 
+                                   
                                 </div>  
+                            </div> 
                             <?php  endforeach; ?> <!-- здесь заканчивается цикл для отображения -->
                         </div>   
-                    </div>
-            </div>
+                   
+           
         </div> 
     </div> 
 <br>
